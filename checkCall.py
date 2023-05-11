fb979d342bef3842826d29c57323264e27b2a3da
Octávio Filipe Pereira <58921861+octaviofilipepereira@users.noreply.github.com>
2023-05-11 20:46:28 +0100

    Update checkCall.py

--- checkCall.py
+++ checkCall.py
@@ -11,7 +11,7 @@ now = datetime.datetime.now()
 serialDevice = "/dev/ttyACM0"
 
 # Target phone number fater validation
-targetPhoneNumber = "000000000"
+targetPhoneNumber = "916247135"
 
 # Open phone device, baudrate and connection timeout
 # Some devices are a little slow getting the initial connection
@@ -36,26 +36,28 @@ try:
         # 2. If this is the case, we need to loop AT+CLCC to check if there is a active Caller_ID 
         if (x == b'RING\r\n'):
             # Connection line is active
-            # Uncomment the following line to debug
-            # print('Ringing - Someone is calling')
+            # Log the ring event
+            print('Ringing - Someone is calling')
             
             # Lets RING one more time on the original traffic subject
             time.sleep(5)
             
+            # Log connection date/time
             print("Connection Date/Time: ", now)
+
             # Run atcom AT commands
             proc = subprocess.Popen(["atcom --port " + str(serialDevice) + " -b 19200 AT+CLCC"], stdout=subprocess.PIPE, shell=True)
             (out, err) = proc.communicate()
             
-            # Debug command object output 
-            # print("Output: ", out)
+            # Log the command object output 
+            print("CID Output: ", out)
             
             # Convert output object to string so we can use it later
             word =  str(out)
             
             # Hangup the call 
-            os.system("atcom --port " + str(serialDevice) + " -b 19200 AT+CHUP")
-            
+            os.system("atcom --port " + str(serialDevice) + " -b 19200 AT+CHUP >/dev/null 2>&1")
+
             # Clear the Caller ID string for unwanted chars 
             new_string = word.replace('\r\n','')
             
@@ -65,14 +67,16 @@ try:
                     line = f.readline()
                     if line.strip():
                         if new_string.find(line.strip()) != -1:
-                            print("Number Found", line.strip())
-                            os.system('atcom --port ' + str(serialDevice) + ' -b 19200 ATDT"' + str(targetPhoneNumber) + ';"')
+                            # Log the event if caller id found
+                            print("Caller ID Found", line.strip())
+                            os.system('atcom --port ' + str(serialDevice) + ' -b 115200 ATDT"' + str(targetPhoneNumber) + ';" >/dev/null 2>&1')
                             time.sleep(10)
-                            os.system("atcom --port " + str(serialDevice) + " -b 19200 AT+CHUP")
+                            os.system("atcom --port " + str(serialDevice) + " -b 19200 AT+CHUP >/dev/null 2>&1")
                     if not line:
                         break
 
             # Wait 3s for the nest loop
+            print("-----------------------\n\n")
             time.sleep(3)
 
 finally:
